8375d578a708a2e9c158ffc80bba0f9ceeec836e
lucas-r-o <lucas.rodrigues@trindadetecnologia.com>
2024-08-08 19:24:19 -0300

    Remove unnecessary classes.

--- Src/Library.GraphQL/Applications/Mutation/IMutation.cs
+++
@@ -1,9 +0,0 @@
-﻿using Library.GraphQL.Models;
-
-namespace Library.GraphQL.Applications.Mutation
-{
-    public interface IMutation
-    {
-        Task<Book> AddBookAsync(string title, string description, Guid authorId);
-    }
-}
