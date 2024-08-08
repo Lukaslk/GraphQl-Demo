58258347a6e6c5fbee9e255cc3e41286dc68cab4
lucas-r-o <lucas.rodrigues@trindadetecnologia.com>
2024-08-08 19:29:36 -0300

    commented line removed.

--- Src/Library.GraphQL/Program.cs
+++ Src/Library.GraphQL/Program.cs
@@ -1,20 +1,18 @@
-using Library.GraphQL.Applications.Query;
 using Library.GraphQL.Applications.Schemas;
+using Library.GraphQL.Extensions;
 using Library.GraphQL.Infrastructure;
 using Library.GraphQL.Repository;
-using Library.GraphQL.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-// Add services to the container.
 builder.Services.AddLibraryDbContext();
 builder.Services.AddBookRepository();
 builder.Services.AddTypes();
-builder.Services.AddQuery();
 builder.Services.AddSchema();
-//builder.Services.AddMutation();
+builder.Services.AddQuery();
+builder.Services.AddMutation();
 
 builder.Services.AddGraphQlCofigServices();
 
