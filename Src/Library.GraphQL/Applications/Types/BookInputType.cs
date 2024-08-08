737ef0a53436ab4a78e42b1a54fabb93514dbff2
lucas-r-o <lucas.rodrigues@trindadetecnologia.com>
2024-08-08 19:30:07 -0300

    Create book type

---
+++ Src/Library.GraphQL/Applications/Types/BookInputType.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+
+namespace Library.GraphQL.Applications.Types
+{
+    public class BookInputType : InputObjectGraphType
+    {
+        public BookInputType()
+        {
+            Field<StringGraphType>("title");
+            Field<StringGraphType>("description");
+            Field< GuidGraphType>("authorId");
+        }
+    }
+}
