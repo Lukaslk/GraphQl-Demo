b8c8fa0cb703876e6b85c8c3e9eecc7c8dc8fda7
lucas-r-o <lucas.rodrigues@trindadetecnologia.com>
2024-08-11 13:22:43 -0300

    Nested query included in book with author

--- Src/Library.GraphQL/Applications/Types/BookType.cs
+++ Src/Library.GraphQL/Applications/Types/BookType.cs
@@ -1,17 +1,24 @@
-﻿using GraphQL.Types;
+﻿using GraphQL;
+using GraphQL.Types;
 using Library.GraphQL.Models;
+using Library.GraphQL.Repository;
 
 namespace Library.GraphQL.Applications.Types
 {
     public class BookType : ObjectGraphType<Book>
     {
-        public BookType()
+        public BookType(IAuthorRepository authorRepository)
         {
             Name = "book";
             Field(p => p.Id);
             Field(p => p.Title);
             Field(p => p.Description);
             Field(p => p.AuthorId);
+            Field<AuthorType>("Author").ResolveAsync(async context =>
+                                        {
+                                            var authorId = context.Source.AuthorId;
+                                            return await authorRepository.GetByIdAsync(authorId);
+                                        });
         }
     }
 }
